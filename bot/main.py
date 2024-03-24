018e6bd499db74860ca9c3bb94e901bf27ca2336
Ivan Kushpel <xkushpel@fi.muni.cz>
2024-03-24 21:16:27 +0100

    feat: add example discord bot

---
+++ bot/main.py
@@ -0,0 +1,24 @@
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+import os
+if not (token := os.getenv("DISCORD_TOKEN")):
+    exit(1)
+
+client.run(token)
